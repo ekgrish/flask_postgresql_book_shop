6a5e1ae2695071c4f6cf168dcfd8aaee075ff50b
Ekaterina Grishatkina <ekaterina.grischatkina@gmail.com>
2019-04-22 01:24:13 +0300

    correction relation may to many: books to authors https://github.com/sqlalchemy/sqlalchemy/blob/master/examples/association/basic_association.py

--- fill_data_helpers.py
+++ fill_data_helpers.py
@@ -4,7 +4,29 @@ from app import db
 
 class SaveDataHelper:
     def new_book(book_dict):
-        return
+        publising_house = PublishingHouse.query.filter_by(name=book_dict['publishing_house_name']).first()
+        if publising_house is None:
+            publising_house = PublishingHouse(book_dict['publishing_house_name'])
+            db.session.add(publising_house)
+            db.session.commit()
+            publising_house_id = PublishingHouse.query.filter_by(name=book_dict['publishing_house_name']).first().id
+        else:
+            publising_house_id = publising_house.id
+
+        book = Book(book_dict['title'],
+                    book_dict['publishing_year'],
+                    book_dict['quantity_in_stock'],
+                    book_dict['description'],
+                    publising_house_id)
+        autors = Author.query.filter_by(name=book_dict['author']).all()
+        if autors is None:
+            autors = Author(book_dict['author'])
+            db.session.add(autors)
+            db.session.commit()
+        #book.book_authors.extend(autors)
+        book.book_authors.append(autors)
+        db.session.add(book)
+        db.session.commit()
 
     def new_magazine(magazine_dict):
         publising_house = PublishingHouse.query.filter_by(name=magazine_dict['publishing_house_name']).first()
--- models.py
+++ models.py
@@ -38,32 +38,24 @@ class Magazine(db.Model):
         return '<Magazine %r>' % self.title
 
 
-author_book_association_table = db.Table('author_book_association',
-                                         db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True),
-                                         db.Column('author_id', db.Integer, db.ForeignKey('author.id'),
-                                                   primary_key=True)
-                                         )
-
-
 class Book(db.Model):
     __tablename__ = 'book'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    publishing_year = db.Column(db.Integer)
-    quantity_in_stock = db.Column(db.Integer)
+    title = db.Column(db.String(120), nullable=False)
+    publishing_year = db.Column(db.Integer, nullable=False)
+    quantity_in_stock = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(400))
-    # author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
-    authors = db.relationship('Author', secondary=author_book_association_table, lazy='subquery',
-                              backref=db.backref('books', lazy=True))
+    book_authors = db.relationship('BookAuthor', cascade="all, delete-orphan", backref="book")
     publishing_house_id = db.Column(db.Integer, db.ForeignKey('publishing_house.id'), nullable=False)
     publishing_house = db.relationship('PublishingHouse', backref=db.backref('books', lazy=True))
 
     # TODO: make using dict
-    def __init__(self, title, publishing_year, quantity_in_stock, description):
+    def __init__(self, title, publishing_year, quantity_in_stock, description, publishing_house_id):
         self.title = title
         self.publishing_year = publishing_year
         self.quantity_in_stock = quantity_in_stock
         self.description = description
+        self.publishing_house_id = publishing_house_id
 
     # TODO: remove, just artefact from lesson or rewrite
     def __repr__(self):
@@ -82,3 +74,15 @@ class Author(db.Model):
     # TODO: remove, just artefact from lesson or rewrite
     def __repr__(self):
         return '<Author %r>' % self.name
+
+
+class BookAuthor(db.Model):
+    __tablename__ = "book_author"
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"), primary_key=True)
+
+    def __init__(self, item, price=None):
+        self.item = item
+        self.price = price or item.price
+
+    author = db.relationship(Author, lazy="joined")
